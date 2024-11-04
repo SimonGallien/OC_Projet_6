332f162745d6c5b5f8c0e6e6e6cd0554bef64836
Simon Gallien <simongallien@orange.fr>
2024-11-04 12:29:41 -0600

    Partie 1 terminé, manque les routes avec rating

---
+++ backend/routes/book.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const bookCtrl = require('../controllers/book');
+
+router.get('/', bookCtrl.getAllBooks);
+router.get('/:id', bookCtrl.getOneBook);
+router.post('/', auth, multer, bookCtrl.createBook);
+router.delete('/:id',auth, bookCtrl.deleteBook);
+router.put('/:id',auth, multer, bookCtrl.modifyBook);
+
+module.exports = router;
