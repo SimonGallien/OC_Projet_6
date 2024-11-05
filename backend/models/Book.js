2b0084634d6f2a8f5ceda7f887e6483b965cc923
Simon Gallien <simongallien@orange.fr>
2024-11-05 17:51:38 -0600

    Valeur ratings et averageRating ont une valeur par défaut [] et 0

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -7,13 +7,16 @@ const bookSchema = mongoose.Schema({
     imageUrl: { type: String, required: true }, //- illustration/couverture du livre
     year: { type: Number, required: true }, //- année de publication du livre
     genre: { type: String, required: true }, //- genre du livre
-    ratings: [
-        {
-            userId: { type: String, required: true }, //- identifiant MongoDB unique de l'utilisateur qui a noté le livre
-            grade: { type: Number, required: true }, //- note donnée à un livre- notes données à un livre
-        },
-    ],
-    averageRating: { type: Number, required: true }, //- note moyenne du livre
+    ratings: {
+        type: [
+            {
+                userId: { type: String, required: true }, //- identifiant MongoDB unique de l'utilisateur qui a noté le livre
+                grade: { type: Number, required: true }, //- note donnée à un livre- notes données à un livre
+            },
+        ],
+        default: [], // Initialisation à un tableau vide
+    },
+    averageRating: { type: Number, required: true, default: 0 }, //- note moyenne du livre
 })
 
 module.exports = mongoose.model('Book', bookSchema)
